f75d34700902deb04b3d12321e5ad78f6e6832b0
Mark-xsx <2143587100@qq.com>
2026-07-11 09:46:04 +0800

    Day 5: FastAPI 连接 Supabase，使用客户端库，添加 .gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from supabase import create_client, Client
 
 app = FastAPI()
+
+# 用你自己的 Project URL 和 anon key 替换下面两个字符串
+SUPABASE_URL = "https://svwpscnnnbghmlqoxtoi.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2d3BzY25ubmJnaG1scW94dG9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NDIxOTAsImV4cCI6MjA5OTIxODE5MH0.bblSB15TH0c8jvRIlIy_XUlzmWPA45ctDVpaqgxI_88"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello Echo"}
-@app.get("/echo/{echo_id}")
-def get_echo(echo_id: int):
-    return {"echo_id": echo_id, "content": f"这是第 {echo_id} 条回声"}
+
+@app.post("/echo")
+def create_echo(content: str):
+    data = supabase.table("echoes").insert({"content": content}).execute()
+    return {"message": "已替你保管"}
+
+@app.get("/echoes")
+def get_all_echoes():
+    response = supabase.table("echoes").select("*").order("created_at", desc=True).execute()
+    return {"echoes": response.data}
